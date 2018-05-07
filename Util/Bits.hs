a95727fa7a43f83b0f4c23bb3b45b8f03f2545ad
M Farkas-Dyck <strake888@gmail.com>
2018-05-06 22:17:40 -0800

    generalize `setBits`

--- Util/Bits.hs
+++ Util/Bits.hs
@@ -1,13 +1,16 @@
 module Util.Bits where
 
+import Control.Applicative
 import Data.Bits
 import Data.Bool
 import Prelude ((==), (+), fromIntegral, id)
 import qualified Prelude
+import Util
 
 (.&¬) :: Bits a => a -> a -> a
 a .&¬ b = a .&. complement b
 
-setBits :: (Bits a, Prelude.Integral n) => a -> [n]
-setBits = go 0 where go n a | zeroBits == a `shiftR` fromIntegral n = []
-                            | True = bool id (n:) (testBit a (fromIntegral n)) (go (n+1) a)
+setBits :: (Bits a, Prelude.Integral n, Alternative f) => a -> f n
+setBits = altMap pure ∘ go 0
+  where go n a | zeroBits == a `shiftR` fromIntegral n = []
+               | True = bool id (n:) (testBit a (fromIntegral n)) (go (n+1) a)
