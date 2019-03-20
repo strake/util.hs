e6d9def2e012727fee3dd0da06f5e0d0b8ead8a9
M Farkas-Dyck <strake888@gmail.com>
2019-03-20 14:56:47 -0800

    padLeft

--- Util/List.hs
+++ Util/List.hs
@@ -2,8 +2,11 @@
 
 module Util.List where
 
+import Control.Applicative
 import Data.Bool
 import Data.List.NonEmpty (NonEmpty (..))
+import Numeric.Natural
+import Util
 
 splitWhen :: (a -> Bool) -> [a] -> NonEmpty [a]
 splitWhen p = go
@@ -11,3 +14,9 @@ splitWhen p = go
     go = \ case
         [] -> []:|[]
         a:(go -> as:|ass) | p a -> []:|as:ass | True -> (a:as):|ass
+
+padLeft :: Natural -> a -> [a] -> [a]
+padLeft n a as = dropLengthOf as (replicate n a) <|> as
+  where dropLengthOf [] as = as
+        dropLengthOf _  [] = []
+        dropLengthOf (_:bs) (_:as) = dropLengthOf bs as
